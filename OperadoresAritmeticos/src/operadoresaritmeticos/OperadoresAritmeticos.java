117a981f57a9e8196c980f21e6a55f2ac0cbf9d8
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-03-15 16:49:42 -0300

    New project OperadoresAritmeticos

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,13 @@
+package operadoresaritmeticos;
+
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1 = 5;
+        int n2 = 3;
+        float media = n1 + n2 / 2;
+        System.out.println("A média é: " + media);
+    }
+    
+}
