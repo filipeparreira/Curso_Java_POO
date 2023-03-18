c3e707d4cfa691af330748766bc1d2b57cff9d80
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-03-18 19:06:58 -0300

    Update ProgramaMedia.java

--- ProgramaMedia/src/programamedia/ProgramaMedia.java
+++ ProgramaMedia/src/programamedia/ProgramaMedia.java
@@ -18,22 +18,34 @@ public class ProgramaMedia {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner teclado = new Scanner(System.in);
-        /*System.out.println("Digite a primeira nota: ");
-        float n1 = teclado.nextFloat();
-        System.out.println("Digite a segunda nota: ");
-        float n2 = teclado.nextFloat();
-        float media = (n1 + n2) / 2;
-        System.out.println("Média: " + media);
-        if(media>7){
-            System.out.println("Parabens!!");
-        }*/
-        System.out.print("Informe seu ano de nascimento: ");
-        int ano = teclado.nextInt();
-        int idade = 2023 - ano;
-        if(idade >= 18){
-            System.out.println("Maior");
+        /*---------------Tira a média de uma nota----------------
+        System.out.println("Digite a primeira nota: ");
+        *   float n1 = teclado.nextFloat();
+        *   System.out.println("Digite a segunda nota: ");
+        *   float n2 = teclado.nextFloat();
+        *   float media = (n1 + n2) / 2;
+        *   System.out.println("Média: " + media);
+        *   if(media>7){
+        *       System.out.println("Parabens!!");
+        *   }
+        */
+        /*---------------Verifica se é maior de idade -----------
+        *   System.out.print("Informe seu ano de nascimento: ");
+        *   int ano = teclado.nextInt();
+        *   int idade = 2023 - ano;
+        *   if(idade >= 18){
+        *       System.out.println("Maior");
+        *   }else{
+        *       System.out.println("Menor");
+        *   }
+        */
+        System.out.print("Digite um numero inteiro: ");
+        int num = teclado.nextInt();
+        int res = num % 2;
+        if (res == 1){
+            System.out.println("O numero digitado não é par :(");
         }else{
-            System.out.println("Menor");
+            System.out.println("O numero digitado é par  :)");
         }
         
     }
