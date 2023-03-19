e00ac6440e330dc74a2553f08ce8c99816ad53ba
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-03-19 15:30:00 -0300

    Update Vetores.java

--- Vetores/src/vetores/Vetores.java
+++ Vetores/src/vetores/Vetores.java
@@ -4,6 +4,8 @@
  */
 package vetores;
 
+import java.util.Arrays;
+
 /**
  *
  * @author filipe
@@ -15,9 +17,31 @@ public class Vetores {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int n[] = {2,8,9,23,5};
-        for (int cont = 0; cont < 5; cont++ ){
-            System.out.println("Posição " + cont + ":" + n[cont]);
+        /*int n[] = {2,8,9,23,5};
+        System.out.println("Tamanho do vetor: " + n.length);
+        for (int cont = 0; cont < n.length; cont++ ){
+            System.out.println("Posição " + cont + ": " + n[cont]);
+        }*/
+        /*String mes[] = {"Jan", "Fev", "Mar", "Abr", "Maio", "Jun", "Jul", "Ago",
+            "Set", "Out", "Nov", "Dez"};
+        int dias[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for (int cont = 0; cont < mes.length; cont++){
+            System.out.println("O mês: " + mes[cont] + " tem " + dias[cont] + " dias");
+        }*/
+        /*int vet[] = {9, 6, 2, 8, 1, -4, 7, 3};
+        for(int valor: vet){
+            System.out.println(valor);
+        }
+        Arrays.sort(vet);
+        for(int valor: vet){
+            System.out.println(valor);
+        }
+        int pos = Arrays.binarySearch(vet, 3);
+        System.out.println(pos);*/
+        int vet[] = new int[20];
+        Arrays.fill(vet, 0);
+        for(int valores: vet){
+            System.out.print(valores +" ");
         }
     }
     
