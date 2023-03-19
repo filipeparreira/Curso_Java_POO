0e21cb91e4552ddf61ce719d2847c98ad3d82b8f
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-03-19 14:48:49 -0300

    Update new project Vetores

---
+++ Vetores/src/vetores/Vetores.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetores;
+
+/**
+ *
+ * @author filipe
+ */
+public class Vetores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n[] = {2,8,9,23,5};
+        for (int cont = 0; cont < 5; cont++ ){
+            System.out.println("Posição " + cont + ":" + n[cont]);
+        }
+    }
+    
+}
