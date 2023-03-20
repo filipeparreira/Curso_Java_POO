05f0d2c07d8d80f57bb0065b5afc696b7e30dbce
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-03-20 16:35:25 -0300

    Update ExercicioEstudante

--- ExercicioEstudante/src/exercicioestudante/Estudante.java
+++ ExercicioEstudante/src/exercicioestudante/Estudante.java
@@ -19,48 +19,55 @@ public class Estudante {
 	private String nome;
 	private String endereco;
 	private String RA;
+	private String sexo;
 	
         //Construtores
 	public Estudante(String nome, String endereco, String RA) { //<--parametro
             this.RA = RA;
             this.endereco = endereco;
             this.nome = nome;		
+            this.sexo = "";
 	}	
         
-        public Estudante(){
-            this.RA = "";
-            this.endereco = "";
-            this.nome = "";
-        }
+    public Estudante(){
+        this.RA = "";
+        this.endereco = "";
+        this.nome = "";
+        this.sexo = "";
+    }
         
-        //Metodos Acessores 
-        public String getNome(){
-            return this.nome;
-        }
-        public String getEnd(){
-            return this.endereco;
-        }
-        public String getRA(){
-            return this.RA;
-        }
-	
+    //Metodos Acessores 
+    public String getNome(){
+        return this.nome;
+    }
+    public String getEnd(){
+        return this.endereco;
+    }
+    public String getRA(){
+        return this.RA;
+    }
+	public String getSexo(){
+	    return this.sexo;
+	}
         
-        //Metodos Mutadores
-        public void setNome(String nome){
-            this.nome = nome;
-        }
-        public void setEnd(String end){
-            this.endereco = end;
-        }
-        public void setRA(String RA){
-            this.RA = RA;
-        }
-	
-        @Override 
-        public String toString(){
-            return "Nome: " + this.nome + "\nRA: " + this.RA + "\nEndereço: " +
-                    this.endereco;
-        }
+    //Metodos Mutadores
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setEnd(String end){
+        this.endereco = end;
+    }
+    public void setRA(String RA){
+        this.RA = RA;
+    }
+	public void setSexo(String sexo){
+	    this.sexo = sexo;
+	}
+    @Override 
+    public String toString(){
+        return "Nome: " + this.nome + "\nRA: " + this.RA + "\nEndereco: " +
+                this.endereco + "\nSexo: " + this.sexo;
+    }
 
 }
 
