9cf0feac521e234973ac1f5f4c9d1f677fb3a0e7
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-04-29 13:52:09 -0300

    Create Getters Setters

--- Aula12/src/aula12/Mamifero.java
+++ Aula12/src/aula12/Mamifero.java
@@ -2,6 +2,14 @@ package aula12;
 
 public class Mamifero extends Animal{
     private String corPelo;
+
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
     
     @Override
     public void locomover(){
