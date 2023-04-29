e4a99e2db22dcced5409f0e5b05c6fde8122c1ed
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-04-29 13:50:52 -0300

    Update Mamifero.java

--- Aula12/src/aula12/Mamifero.java
+++ Aula12/src/aula12/Mamifero.java
@@ -1,5 +1,18 @@
 package aula12;
 
-public class Mamifero {
+public class Mamifero extends Animal{
+    private String corPelo;
     
+    @Override
+    public void locomover(){
+        System.out.println("Correndo");
+    }
+    @Override
+    public void alimentar(){
+        System.out.println("Mamando");
+    }
+    @Override
+    public void emitirSom(){
+        System.out.println("Som de Mamífero");
+    }
 }
